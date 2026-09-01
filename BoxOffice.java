8400319c3c15bcac0694692c378b48c89972f899
trumpercharles <trumper.charles@student.ccm.edu>
2026-09-01 14:36:50 -0400

    Complete Week 1 Lab 1

--- BoxOffice.java
+++ BoxOffice.java
@@ -1,4 +1,34 @@
+import java.util.Scanner;
+
 public class BoxOffice {
-    
-    
+    public static void main(String args[]){
+        Scanner newScanner=new Scanner(System.in);
+
+        String Name;
+        int Children;
+        int Adults;
+
+        System.out.print("What is the name of the movie you'd like to see: ");
+        Name=newScanner.nextLine();
+
+        System.out.print("\n\nHow many adult tickets would you like to buy: ");
+        Adults=newScanner.nextInt();
+
+        System.out.print("\n\nHow many children tickets would you like to buy: ");
+        Children=newScanner.nextInt();
+
+        double childrenCost=6;
+        double adultCost=10;
+
+        double grossBoxOfficeProfit=(childrenCost*Children)+(adultCost+Adults);
+        double netBoxOfficeProfit=grossBoxOfficeProfit/5;
+        double distributorProfit=grossBoxOfficeProfit-netBoxOfficeProfit;
+
+        System.out.print("\n\nMovie Name:                   "+Name+
+                           "\nAdult Tickets Sold:           "+Adults+
+                           "\nChild Tickets Sold:           "+Children+
+                           "\nGross Box Office Profit:      $"+grossBoxOfficeProfit+
+                           "\nNet Box Office Profit:        $"+netBoxOfficeProfit+
+                           "\nAmount Paid to Distributor:   $"+distributorProfit);
+    }
 }
